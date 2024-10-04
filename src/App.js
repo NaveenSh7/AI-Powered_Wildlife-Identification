d26046f75bc610d78df72b704db01d7b2494db38
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-10-05 04:30:30 +0530

    Added Css

--- src/App.js
+++ src/App.js
@@ -1,187 +1,30 @@
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import * as mobilenet from "@tensorflow-models/mobilenet";
-import * as tf from '@tensorflow/tfjs';
-
+import {Route, Routes, useLocation } from 'react-router-dom';
+import Home from './Home.js'
+import Navbar from './Navbar.js'
+import Footer from './Footer.js'
 function App() {
-    const [isModelLoading, setIsModelLoading] = useState(false);
-    const [model, setModel] = useState(null);
-    const [imageURL, setImageURL] = useState(null);
-    const [results, setResults] = useState([]);
-    const [history, setHistory] = useState([]);
-
-    const [animalName, setAnimalName] = useState('');
-    const [animalInfo, setAnimalInfo] = useState('');
-
-    const imageRef = useRef();
-    const textInputRef = useRef();
-    const fileInputRef = useRef();
-
-
-    const setTensorFlowBackend = async () => {
-        try {
-            await tf.setBackend('webgl');  // You can also try 'cpu' or 'wasm'
-            console.log('TensorFlow.js backend set to WebGL');
-        } catch (error) {
-            console.error('Error setting backend:', error);
-        }
-    };
-
-    const loadModel = async () => {
-        await setTensorFlowBackend();  // Ensure backend is set before loading the model
-        setIsModelLoading(true);
-        try {
-            const model = await mobilenet.load();
-            setModel(model);
-            setIsModelLoading(false);
-            console.log("Model loaded successfully!");
-        } catch (error) {
-            console.error("Error loading model:", error);
-            setIsModelLoading(false);
-        }
-    };
-
-
-    const uploadImage = (e) => {
-        const { files } = e.target;
-        if (files.length > 0) {
-            const url = URL.createObjectURL(files[0]);
-            setImageURL(url);
-        } else {
-            setImageURL(null);
-        }
-    };
-
-    const identify = async () => {
-        textInputRef.current.value = '';
-        if (model && imageRef.current) {
-            try {
-                const results = await model.classify(imageRef.current);
-                setResults(results);
-                setAnimalName(results[0].className);
-
-            } catch (error) {
-                console.error('Error during classification:', error);
-            }
-        } else {
-            console.error('Model is not loaded yet or image is missing');
-        }
-    };
-
-    const handleOnChange = (e) => {
-        setImageURL(e.target.value);
-        setResults([]);
-    };
-
-    const triggerUpload = () => {
-        fileInputRef.current.click();
-    };
-
-    useEffect(() => {
-        loadModel();
-    }, []);
-
-    useEffect(() => {
-        if (imageURL) {
-            setHistory((prevHistory) => [imageURL, ...prevHistory].slice(0, 5));
-        }
-    }, [imageURL]);
-
-    if (isModelLoading) {
-        return <h2>Model Loading...</h2>;
-    }
-    //capitalize
-
-
-    const capitalizeFirstLetterOfEachWord = (input) => {
-        const firstName = input.split(',')[0].trim();
-        return firstName.split(' ')
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(' ');
-    };
     
-    //Wiki api
-
-    const fetchAnimalInfo = async () => {
-        if (animalName === '') {
-            return (<p>No animal</p>)
-        }
-        const namesArray = animalName.split(',').map(name => name.trim());
-        console.log(namesArray)
-        //  const title = capitalizeFirstLetterOfEachWord(animalName);
-        const title = namesArray[0];
-
-        const url = `https://en.wikipedia.org/w/api.php?action=query&titles=${title}&prop=extracts|pageimages&exintro&explaintext&format=json&origin=*`;
-        console.log(url)
-        const response = await axios.get(url);
-
-        const page = response.data.query.pages;
-        const pageId = Object.keys(page)[0];
-        setAnimalInfo(page[pageId].extract)
-        console.log(page[pageId].extract)
-
-    };
-
 
 
 
 
     return (
-        <div className="App">
-            <h1 className="header">AI-Powered Wildlife Identification</h1>
-            <div className="inputHolder">
-                <input type="file" accept="image/*" capture="camera" className="uploadInput" onChange={uploadImage} ref={fileInputRef} />
-                <button className="uploadImage" onClick={triggerUpload}>Upload Image</button>
-                <span className="or">OR</span>
-                <input type="text" placeholder="Paste image URL" ref={textInputRef} onChange={handleOnChange} />
-            </div>
-            <div className="mainWrapper">
-                <div className="mainContent">
-                    <div className="imageHolder">
-                        {imageURL && <img src={imageURL} alt="Upload Preview" crossOrigin="anonymous" ref={imageRef} style={{ width: '224px', height: '224px' }} />}
-                    </div>
-                    {results.length > 0 && (
-                        <div className="resultsHolder">
-                            {results.map((result, index) => (
-                                <div className="result" key={result.className}>
-                                    <span className="name">{result.className}</span>
-                                    <span className="confidence">Confidence level: {(result.probability * 100).toFixed(2)}% {index === 0 && <span className="bestGuess">Best Guess</span>}</span>
-                                </div>
-                            ))}
-                        </div>
-                    )}
-                </div>
-                <button className="button" onClick={identify} disabled={isModelLoading || !imageURL}>
-                    {isModelLoading ? 'Loading Model...' : 'Identify Image'}
-                </button>
-            </div>
-            <div>
-                Name : {animalName}
-            </div>
-            <button className="button" onClick={() => { fetchAnimalInfo(animalName) }} disabled={isModelLoading || !animalName} >
-                Get Info
-            </button>
+        <div>
+     <Navbar/>
+     
+     <Routes>
 
-            {animalInfo && (
-                <div className="animalInfo">
-                    <h3>Animal Info:</h3>
-                    <p>{animalInfo}</p>
-                </div>
-            )}
+        <Route exact path='/' element={ <Home/>}/>
 
-            {/* {history.length > 0 && (
-                <div className="recentPredictions">
-                    <h2>Recent Images</h2>
-                    <div className="recentImages">
-                        {history.map((image, index) => (
-                            <div className="recentPrediction" key={`${image}${index}`}>
-                                <img src={image} alt="Recent Prediction" onClick={() => setImageURL(image)} />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            )} */}
+     </Routes>
+
+     <Footer/>
         </div>
+
+     
+   
     );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,181 @@
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as tf from '@tensorflow/tfjs';
+import './Loader.css'
+const Home = () => {
+    const [isModelLoading, setIsModelLoading] = useState(false);
+    const [model, setModel] = useState(null);
+    const [imageURL, setImageURL] = useState(null);
+    const [results, setResults] = useState([]);
+    const [history, setHistory] = useState([]);
+    const [animalName, setAnimalName] = useState('');
+    const [animalInfo, setAnimalInfo] = useState('');
+
+    const imageRef = useRef();
+    const textInputRef = useRef();
+    const fileInputRef = useRef();
+
+    const setTensorFlowBackend = async () => {
+        try {
+            await tf.setBackend('webgl');
+            console.log('TensorFlow.js backend set to WebGL');
+        } catch (error) {
+            console.error('Error setting backend:', error);
+        }
+    };
+
+    const loadModel = async () => {
+        await setTensorFlowBackend();
+        setIsModelLoading(true);
+        try {
+            const model = await mobilenet.load();
+            setModel(model);
+            setIsModelLoading(false);
+            console.log("Model loaded successfully!");
+        } catch (error) {
+            console.error("Error loading model:", error);
+            setIsModelLoading(false);
+        }
+    };
+
+    const uploadImage = (e) => {
+        const { files } = e.target;
+        if (files.length > 0) {
+            const url = URL.createObjectURL(files[0]);
+            setImageURL(url);
+        } else {
+            setImageURL(null);
+        }
+    };
+
+    const identify = async () => {
+        textInputRef.current.value = '';
+        if (model && imageRef.current) {
+            try {
+                const results = await model.classify(imageRef.current);
+                setResults(results);
+                setAnimalName(results[0].className);
+            } catch (error) {
+                console.error('Error during classification:', error);
+            }
+        } else {
+            console.error('Model is not loaded yet or image is missing');
+        }
+    };
+
+    const handleOnChange = (e) => {
+        setImageURL(e.target.value);
+        setResults([]);
+    };
+
+    const triggerUpload = () => {
+        fileInputRef.current.click();
+    };
+
+    useEffect(() => {
+        loadModel();
+    }, []);
+
+    useEffect(() => {
+        if (imageURL) {
+            setHistory((prevHistory) => [imageURL, ...prevHistory].slice(0, 5));
+        }
+    }, [imageURL]);
+
+    const capitalizeFirstLetterOfEachWord = (input) => {
+        const firstName = input.split(',')[0].trim();
+        return firstName.split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
+    };
+
+    const fetchAnimalInfo = async () => {
+        if (animalName === '') {
+            return (<p>No animal</p>);
+        }
+        const namesArray = animalName.split(',').map(name => name.trim());
+        const title = namesArray[1];
+
+        const url = `https://en.wikipedia.org/w/api.php?action=query&titles=${title}&prop=extracts|pageimages&exintro&explaintext&format=json&origin=*`;
+        const response = await axios.get(url);
+
+        const page = response.data.query.pages;
+        const pageId = Object.keys(page)[0];
+        setAnimalInfo(page[pageId].extract);
+    };
+
+    if (isModelLoading) {
+        return (
+            <div className=" h-96 w-full flex justify-center items-center mb-16 mt-16 ">
+                <div className="flex flex-col items-center">
+                    {/* Adjusted loader size */}
+                    <div className="loader w-24 h-64 mb-4"></div> 
+                    <div className="text-2xl font-semibold">Loading . . . .</div>
+                </div>
+            </div>
+        );
+    }
+    
+    
+    
+
+    return (
+        <div className="App flex flex-col items-center bg-[#E9EFEC] p-8 min-h-screen">
+            <h1 className="text-4xl font-bold text-gray-800 mb-8">AI-Powered Wildlife Identification</h1>
+
+            <div className="inputHolder flex flex-col items-center mb-8 space-y-4">
+                <input type="file" accept="image/*" capture="camera" className="hidden" onChange={uploadImage} ref={fileInputRef} />
+                <button className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600" onClick={triggerUpload}>
+                    Upload Image
+                </button>
+                <span className="text-gray-600 text-sm">OR</span>
+                <input type="text" placeholder="Paste image URL" ref={textInputRef} onChange={handleOnChange} className="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-green-500" />
+            </div>
+
+            <div className="mainWrapper flex flex-col items-center">
+                <div className="mainContent">
+                    <div className="imageHolder mb-4">
+                        {imageURL && (
+                            <img src={imageURL} alt="Upload Preview" ref={imageRef} className="w-56 h-56 rounded-md shadow-lg" />
+                        )}
+                    </div>
+                    {results.length > 0 && (
+                        <div className="resultsHolder grid grid-cols-1 sm:grid-cols-3 gap-4 mt-6">
+                            {results.slice(0, 3).map((result, index) => (
+                                <div key={result.className} className="bg-white p-4 rounded-lg shadow-md text-center">
+                                    <span className="block text-xl font-bold text-gray-700">{result.className}</span>
+                                    <span className="text-sm text-gray-500">
+                                        Confidence level: {(result.probability * 100).toFixed(2)}%
+                                        {index === 0 && <span className="text-green-500"> (Best Guess)</span>}
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <button className="relative h-10 w-36 overflow-hidden border border-green-600 text-green-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-green-600 before:duration-300 before:ease-out hover:text-white hover:shadow-green-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80" onClick={identify} disabled={isModelLoading || !imageURL}>
+                <span class="relative z-10">Identify !</span>
+                </button>
+            </div>
+
+            <div className="mt-8 text-lg">
+                <p className="font-bold">Name:</p> 
+                {animalName}
+            </div>
+            <button className="relative h-10 w-36 overflow-hidden border border-green-600 text-indigo-600 shadow-2xl transition-all duration-200 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-indigo-600 before:duration-300 before:ease-out hover:text-white hover:shadow-indigo-600 hover:before:h-40 hover:before:w-40 hover:before:opacity-80" onClick={() => { fetchAnimalInfo(animalName) }} disabled={isModelLoading || !animalName} >
+            <span class="relative z-10"> Get Info</span>
+              
+            </button>
+
+            {animalInfo && (
+                <div className="animalInfo mt-6 bg-white p-4 rounded-md shadow-md">
+                    <h3 className="text-2xl font-bold text-gray-700 mb-2">Animal Info:</h3>
+                    <p className="text-gray-600">{animalInfo}</p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Home;
