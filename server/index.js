3748c9324acfb06a186bdd009559cef9b9fec269
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-10-05 13:25:54 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
-<<<<<<< HEAD
 const mongoURI = "mongodb+srv://naveensharalayya:qhGfWNl95h1yyVty@cluster0.eg6nx.mongodb.net/test";
 
 
@@ -16,8 +15,6 @@ mongoose.connect(mongoURI, {
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log('MongoDB Connection Error: ', err));
 
-=======
->>>>>>> c7f8f2a4878acfb3be795f1f561aac9ca69a2b4c
 
 const PORT = 5000;
 
