fe44ebc7618473111c2a620a81094680d1b0101c
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-12-05 02:31:40 +0530

    .env

--- server/Coudinary.js
+++ server/Coudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({ 
-    cloud_name: 'dihb2u4ni', 
-    api_key: '384272935651332', 
-    api_secret: '6oPO7ey8a7aPuuNSwKznOueD80M' // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET// Click 'View API Keys' above to copy your API secret
 });
 
 module.exports = cloudinary;
