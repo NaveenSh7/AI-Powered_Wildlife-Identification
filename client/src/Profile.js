514dc1cca08acdc7ca29a8e05b25aa548dfc5339
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-12-19 22:23:30 +0530

    Update Profile.js

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from 'axios';
 import { useAuth0 } from "@auth0/auth0-react";
 
